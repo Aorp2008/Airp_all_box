2a947721d0208209712375ebbbb1cc9b77ff94d5
Aorp2008 <123627593+Aorp2008@users.noreply.github.com>
2024-07-03 09:47:19 +0800

    Update updateOpenSSH.sh

--- updateOpenSSH.sh
+++ updateOpenSSH.sh
@@ -4,6 +4,7 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "请使用root权限运行此脚本。"
     exit 1
 fi
+
 if command -v apt-get > /dev/null; then
     package_manager="apt"
 elif command -v yum > /dev/null; then
@@ -12,23 +13,25 @@ else
     echo "不支持的系统类型 仅支持APT和YUM软件包管理"
     exit 1
 fi
+
 if [ "$package_manager" = "apt" ]; then
-    apt-get update
-    apt-get upgrade -y
-    apt-get install -y make gcc git autoconf libssl-dev zlib1g-dev
+    apt-get update > /dev/null
+    apt-get upgrade -y > /dev/null
+    apt-get install -y make gcc git autoconf libssl-dev zlib1g-dev > /dev/null
 elif [ "$package_manager" = "yum" ]; then
-    yum update -y
-    yum upgrade -y
-    yum install -y make gcc git autoconf openssl-devel zlib-devel
+    yum update -y > /dev/null
+    yum upgrade -y > /dev/null
+    yum install -y make gcc git autoconf openssl-devel zlib-devel > /dev/null
 fi
+
 temp_dir=$(mktemp -d)
-git clone https://github.com/openssh/openssh-portable "$temp_dir"
+git clone https://github.com/openssh/openssh-portable "$temp_dir" > /dev/null 2>&1
 cd "$temp_dir" || exit
-autoreconf
-./configure
-make
-make install
+autoreconf > /dev/null 2>&1
+./configure > /dev/null 2>&1
+make > /dev/null 2>&1
+make install > /dev/null 2>&1
 rm -rf "$temp_dir"
-source /etc/profile
+. /etc/profile
 ssh -V
 echo "OpenSSH升级成功"
